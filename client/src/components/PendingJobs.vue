97e87f19fb35486a4124425dc8e2902c2fba6bfa
Ricardo Münch <munch_141@hotmail.com>
2022-06-26 14:33:50 +0200

    Simplify things

---
+++ client/src/components/PendingJobs.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Pending jobs, update</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PendingJobs',
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
